add1ac4b98cecaa5f2a7c5a68777becd1fcdf493
Antek2007 <147153458+Antek2007@users.noreply.github.com>
2025-10-22 16:37:50 +0200

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+    // Na Starcie
+
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // Co klatke
     void Update()
     {
-        
+        // czy klinal spacje
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up*400f);
+        }
     }
 }
