39998907ea1ee87a159cc0f4057546bdbbaab634
Antek2007 <147153458+Antek2007@users.noreply.github.com>
2025-11-19 16:34:40 +0100

    Ground detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public LayerMask groundMask;
+    public float length = 0.6f;
+
     private Rigidbody2D rb;
     void Start()
     {
@@ -10,14 +13,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
             Debug.Log("Skok!");
             Jump();
         }
     }
 
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,length, groundMask);
+        return hit.collider != null;
+    }
+
     void Jump()
     {
         rb.AddForce(Vector2.up * 400f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * length);
     }
 }
