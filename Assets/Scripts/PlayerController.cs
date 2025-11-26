65b9d760621a3a69c06fcc455a47ab9e18060976
Antek2007 <147153458+Antek2007@users.noreply.github.com>
2025-11-26 16:36:52 +0100

    player jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,46 @@
 using UnityEngine;
 
+/*
+ ====================
+ TO DO: 
+force change to linear velocity
+timeout for jump
+
+ ====================
+ */
+
 public class PlayerController : MonoBehaviour
 {
     public LayerMask groundMask;
     public float length = 0.6f;
+    public int maxJumps = 2;
+
+    private int jumpCount = 1;
+
+    private Vector3 startScale;
 
     private Rigidbody2D rb;
     void Start()
     {
+        startScale = transform.localScale; //skala startowa gracza
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Debug.Log("Kucniecie!");
+
+            //transform.localScale 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount<maxJumps) {
             Debug.Log("Skok!");
             Jump();
         }
+
+        if (IsGrounded()) jumpCount = 1;
     }
 
     bool IsGrounded()
@@ -27,6 +51,7 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
+        jumpCount++; 
         rb.AddForce(Vector2.up * 400f);
     }
 
