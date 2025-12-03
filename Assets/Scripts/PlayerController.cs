11b33e4a152ea9982b177d941dcd055c9c9bcea9
Antek2007 <147153458+Antek2007@users.noreply.github.com>
2025-12-03 16:43:22 +0100

    jump fix, crouch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,25 +14,39 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundMask;
     public float length = 0.6f;
     public int maxJumps = 2;
+    [SerializeField] float crouchScaleY = 0.5f;//!!!
 
     private int jumpCount = 1;
 
+    [SerializeField] float jumpForce = 12;
+
     private Vector3 startScale;
+    Vector3 crouchScale;//!!!
+
+    bool isCrouching = false;
 
     private Rigidbody2D rb;
     void Start()
     {
         startScale = transform.localScale; //skala startowa gracza
         rb = GetComponent<Rigidbody2D>();
+        crouchScale = new Vector3(startScale.x, startScale.y * crouchScaleY, startScale.z);//skala crouch gracza
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+
+
+        if (Input.GetKey(KeyCode.LeftControl) && !isCrouching)
         {
             Debug.Log("Kucniecie!");
+            StartCrouching();
+        }
 
-            //transform.localScale 
+        if(Input.GetKeyUp(KeyCode.LeftControl) && isCrouching)
+        {
+            Debug.Log("Nie kuca");
+            StopCrouching();
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount<maxJumps) {
@@ -43,6 +57,20 @@ public class PlayerController : MonoBehaviour
         if (IsGrounded()) jumpCount = 1;
     }
 
+    void StartCrouching()
+    {
+        isCrouching = true;
+        transform.localScale = crouchScale;
+        transform.localPosition -= new Vector3(0, startScale.y * crouchScaleY, 0);
+    }
+
+    void StopCrouching()
+    {
+        isCrouching = false;
+        transform.localScale = startScale;
+        transform.localPosition += new Vector3(0, startScale.y * crouchScaleY, 0);
+    }
+
     bool IsGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,length, groundMask);
@@ -51,8 +79,8 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        jumpCount++; 
-        rb.AddForce(Vector2.up * 400f);
+        jumpCount++;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 
     private void OnDrawGizmos()
