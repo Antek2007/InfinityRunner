8d7e1cb5c31894d9f300621e8147ddcc3ab77254
Antek2007 <147153458+Antek2007@users.noreply.github.com>
2026-03-04 16:34:23 +0100

    PlayerHealth

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 3;
+    public int maxHealth = 3; // Max ilosc serc
+
+    // Canvas i ikony serc
+    // smierc gracza
+    // serca do zbierania
+
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            TakeDamage(1); // strac serce
+        }
+
+        if (collision.CompareTag("Heal"))
+        {
+            Heal(1); // dodaj serce
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage; // health = health - damage;
+        if (health <= 0) Debug.Log("Przegrales!");
+    }
+
+    public void Heal(int amount)
+    {
+        health += amount; // health = health + amount;
+        health = Mathf.Clamp(health, 0, maxHealth);
+    }
+}
